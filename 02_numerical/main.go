54bfed01a575c7aa9afaaf7f8efe98035cba0b99
fishbroetchn <philipp.schirakowski@gmail.com>
2017-09-18 21:59:30 +0200

    exersises here we come

--- 02_numerical/main.go
+++ 02_numerical/main.go
@@ -3,9 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	//fmt.Printf("%d \t %b \t %x \n", 42, 42, 42)
 
-	for i := 0; i < 200; i++ {
-		fmt.Printf("%03d \t %08b \t %02x  \t %q\n", i, i, i, i)
-	}
+	//First exercise
+
+	fmt.Println("Hello World.")
+
 }
